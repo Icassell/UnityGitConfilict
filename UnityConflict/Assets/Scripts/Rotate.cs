1b75d644f4a104a32af534f4de42dacd7215e184
田野 <icassell0717@gmail.com>
2021-03-03 15:40:10 +0800

    master修改脚本

--- UnityConflict/Assets/Scripts/Rotate.cs
+++ UnityConflict/Assets/Scripts/Rotate.cs
@@ -7,7 +7,7 @@ public class Rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("123456");
     }
 
     // Update is called once per frame
